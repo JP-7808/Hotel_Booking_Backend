bbb48152db82093c6c43bbf460e6441bb5207f6d
Jay Prakash <jayp7808@gmail.com>
2024-10-11 15:08:15 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,70 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import authRoute from './routes/auth.js';
+import usersRoute from './routes/users.js';
+import roomsRoute from './routes/rooms.js';
+import hotelsRoute from './routes/hotels.js';
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to MongoDB.");
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB disconnected");
+});
+
+// Middleware
+app.use(cors({
+    origin: ['https://hotel-booking-full-stack-client.vercel.app', 'https://hotel-booking-full-stack-admin.vercel.app'],
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+});
+
+// Health Check Route
+app.get("/", async (req, res) => {
+    try {
+        await mongoose.connection.db.admin().ping();
+        res.send("Backend connected to MongoDB and running on Vercel");
+    } catch (error) {
+        res.status(500).send("MongoDB connection error");
+    }
+});
+
+// Start Server
+const PORT = process.env.PORT || 8800; // Use the port from the environment or default to 8800
+app.listen(PORT, () => {
+    connect();
+    console.log("Connected to backend");
+});
